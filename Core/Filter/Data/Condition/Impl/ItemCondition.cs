2af1b877c0df9f0b514a269a37c7bd74df50225f
kapehh-karen <git@kapehh.net>
2017-12-14 15:38:14 +0300

    upd

--- Core/Filter/Data/Condition/Impl/ItemCondition.cs
+++ Core/Filter/Data/Condition/Impl/ItemCondition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Core.Filter.Data.Condition
+namespace Core.Filter.Data.Condition.Impl
 {
     public class ItemCondition : ICondition
     {
--- Core/Filter/Data/Operand/Impl/FieldOperand.cs
+++ Core/Filter/Data/Operand/Impl/FieldOperand.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Core.Filter.Data.Operand
+namespace Core.Filter.Data.Operand.Impl
 {
     public class FieldOperand : IFilterOperand
     {
