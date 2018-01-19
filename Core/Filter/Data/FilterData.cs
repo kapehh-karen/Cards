ba6bd0c0adc82b0ce22fcd8e3a2a26ec593e3845
kapehh-karen <git@kapehh.net>
2018-01-19 15:03:48 +0300

    up

--- Core/Filter/Data/FilterData.cs
+++ Core/Filter/Data/FilterData.cs
@@ -10,9 +10,11 @@ namespace Core.Filter.Data
     public class FilterData
     {
         public FilterTable FilterTable { get; set; } = null;
+        
+        public ICondition Where { get; set; } = new ContainerCondition();
 
         public FilterData Parent { get; set; } = null;
 
-        public ICondition Where { get; set; } = new ContainerCondition();
+        public List<FilterData> Chields { get; set; } = new List<FilterData>();
     }
 }
