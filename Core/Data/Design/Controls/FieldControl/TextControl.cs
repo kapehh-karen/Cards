314d8ed5156865e28497f21051f43997b5dfa16e
kapehh-karen <git@kapehh.net>
2018-04-21 23:10:11 +0300

    MaxLengthProperty no need more

--- Core/Data/Design/Controls/FieldControl/TextControl.cs
+++ Core/Data/Design/Controls/FieldControl/TextControl.cs
@@ -20,7 +20,6 @@ namespace Core.Data.Design.Controls.FieldControl
             Properties.Add(new FontProperty(this));
             Properties.Add(new PositionProperty(this));
             Properties.Add(new FieldProperty(this) { AccessTypes = new FieldType[] { FieldType.TEXT, FieldType.NUMBER } });
-            Properties.Add(new MaxLengthProperty(this));
             Properties.Add(new ReadOnlyProperty(this));
             Properties.Add(new TabIndexProperty(this));
             
