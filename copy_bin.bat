6e3ea98c1f237d4840f93cababb6fc90e5f08319
kapehh-karen <kapehh-karen@users.noreply.github.com>
2018-11-05 16:53:44 +0300

    copy release

--- copy_bin.bat
+++ copy_bin.bat
@@ -4,7 +4,7 @@ IF NOT EXIST BIN (
 MKDIR BIN
 )
 
-XCOPY /Y Core\bin\x86\Debug\Core.dll BIN
-XCOPY /Y Cards\bin\x86\Debug\Cards.exe BIN
-XCOPY /Y Registration\bin\x86\Debug\Registration.exe BIN
-XCOPY /Y Settings\bin\x86\Debug\Settings.exe BIN
+XCOPY /Y Core\bin\x86\Release\Core.dll BIN
+XCOPY /Y Cards\bin\x86\Release\Cards.exe BIN
+XCOPY /Y Registration\bin\x86\Release\Registration.exe BIN
+XCOPY /Y Settings\bin\x86\Release\Settings.exe BIN
