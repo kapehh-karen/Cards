90a0a0caea12a73cb258e80ca05e52be8cf6bb03
Karen <git@kapehh.net>
2020-11-29 00:25:11 +0300

    Add clean

---
+++ clean.bat
@@ -0,0 +1,11 @@
+RMDIR /S /Q "Cards/bin"
+RMDIR /S /Q "Cards/obj"
+
+RMDIR /S /Q "Core/bin"
+RMDIR /S /Q "Core/obj"
+
+RMDIR /S /Q "Registration/bin"
+RMDIR /S /Q "Registration/obj"
+
+RMDIR /S /Q "Settings/bin"
+RMDIR /S /Q "Settings/obj"
