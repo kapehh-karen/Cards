44291ab87625593f64bab9e8a184554ebe1ff3d4
Karen <git@kapehh.net>
2020-11-29 15:31:48 +0300

    Fix proj

--- copy_bin.bat
+++ copy_bin.bat
@@ -4,7 +4,7 @@ IF NOT EXIST BIN (
 MKDIR BIN
 )
 
-XCOPY /Y Core\bin\x86\Release\Core.dll BIN
-XCOPY /Y Cards\bin\x86\Release\Cards.exe BIN
-XCOPY /Y Registration\bin\x86\Release\Registration.exe BIN
-XCOPY /Y Settings\bin\x86\Release\Settings.exe BIN
+XCOPY /Y Core\bin\x86\Release\net40\Core.dll BIN
+XCOPY /Y Cards\bin\x86\Release\net40\Cards.exe BIN
+XCOPY /Y Registration\bin\x86\Release\net40\Registration.exe BIN
+XCOPY /Y Settings\bin\x86\Release\net40\Settings.exe BIN
